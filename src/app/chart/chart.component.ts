fbfb544b9bebca11547b097fc1744860a5a933e3
Andreas <mchoffn@hotmail.com>
2024-05-27 16:54:41 +0200

    first look at the project

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -2,81 +2,81 @@ import { Component, Input } from '@angular/core';
 import * as Highcharts from 'highcharts';
 
 @Component({
-  selector: 'app-chart',
-  templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.scss'],
+    selector: 'app-chart',
+    templateUrl: './chart.component.html',
+    styleUrls: ['./chart.component.scss'],
 })
 export class ChartComponent {
-  Highcharts: typeof Highcharts = Highcharts;
+    Highcharts: typeof Highcharts = Highcharts;
 
-  @Input() timeseriesData: [number, number][] = [];
-  @Input() downsample: boolean = false;
+    @Input() timeseriesData: [number, number][] = [];
+    @Input() downsample: boolean = false;
 
-  constructor() {}
+    constructor() { }
 
-  chartInitialized(chart: Highcharts.Chart): void {
-    const data: [number, number][] = !this.downsample
-      ? this.timeseriesData
-      : this.returnDownsampledData();
+    chartInitialized(chart: Highcharts.Chart): void {
+        const data: [number, number][] = !this.downsample
+            ? this.timeseriesData
+            : this.returnDownsampledData();
 
-    chart.series[0].setData(data);
-  }
+        chart.series[0].setData(data);
+    }
 
-  chartOptions: Highcharts.Options = {
-    chart: {
-      type: 'line',
-    },
-    title: {
-      text: '',
-    },
-    xAxis: {
-      type: 'datetime', // Use datetime type for time series
-      labels: {
-        enabled: false, // Disable x-axis labels
-      },
-    },
-    yAxis: {
-      title: {
-        text: null, // Remove y-axis title
-      },
-    },
-    legend: {
-      enabled: false, // Disable the legend
-    },
-    series: [
-      {
-        type: 'line',
-        lineWidth: 1,
-      },
-    ],
-    plotOptions: {
-      series: {
-        states: {
-          hover: {
-            lineWidthPlus: 0, // Prevent line from growing bold on hover
-          },
+    chartOptions: Highcharts.Options = {
+        chart: {
+            type: 'line',
         },
-      },
-    },
-    credits: {
-      enabled: false,
-    },
-  };
+        title: {
+            text: '',
+        },
+        xAxis: {
+            type: 'datetime', // Use datetime type for time series
+            labels: {
+                enabled: false, // Disable x-axis labels
+            },
+        },
+        yAxis: {
+            title: {
+                text: null, // Remove y-axis title
+            },
+        },
+        legend: {
+            enabled: false, // Disable the legend
+        },
+        series: [
+            {
+                type: 'line',
+                lineWidth: 1,
+            },
+        ],
+        plotOptions: {
+            series: {
+                states: {
+                    hover: {
+                        lineWidthPlus: 0, // Prevent line from growing bold on hover
+                    },
+                },
+            },
+        },
+        credits: {
+            enabled: false,
+        },
+    };
 
-  /** Modify and extend the code where indicated. You're also allowed to add utility functions below.*/
-  private returnDownsampledData(): [number, number][] {
-    console.time('returnDownsampledData');
-    const dataToDownsample: [number, number][] = [...this.timeseriesData];
-    const plotWidth: number = 548;
+    /** Modify and extend the code where indicated. You're also allowed to add utility functions below.*/
+    private returnDownsampledData(): [number, number][] {
+        console.time('returnDownsampledData');
+        const dataToDownsample: [number, number][] = [...this.timeseriesData];
+        const plotWidth: number = 548;
 
-    // <-- Modify start
-    const downsampledData: [number, number][] = dataToDownsample;
-    // --> Modify end
+        // <-- Modify start
+        const downsampledData: [number, number][] = dataToDownsample;
+        // --> Modify end
 
-    console.timeEnd('returnDownsampledData');
-    console.log(
-      `Input ${this.timeseriesData.length}, Output ${downsampledData.length}`
-    );
-    return downsampledData;
-  }
+        console.timeEnd('returnDownsampledData');
+        console.log(
+            `Input ${this.timeseriesData.length}, Output ${downsampledData.length}`
+        );
+        return downsampledData;
+    }
 }
